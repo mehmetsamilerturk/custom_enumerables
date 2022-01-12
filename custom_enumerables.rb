73e8c226e7a96a3845816ea8746df7b824da6a4b
Mehmet Samil Erturk <mehmetsamilerturk@outlook.com>
2022-01-13 00:45:27 +0300

    add initial files, #my_each done

---
+++ custom_enumerables.rb
@@ -0,0 +1,20 @@
+module Enumerable
+  def my_each
+    each do |i|
+      yield(self[index(i)])
+    end
+  end
+end
+
+def compare_each
+  puts 'my_each vs. each'
+  numbers = [1, 2, 3, 4, 5]
+  a = numbers.my_each { |item| p item }
+  puts
+  b = numbers.each { |item| p item }
+
+  p "my_each: #{a}"
+  p "each: #{b}"
+end
+
+#compare_each
